86a9cb7a744591b2380381e7ffb5afd920a7d92e
Artem Sofin <sofinan@rambler.ru>
2020-08-05 12:02:04 +0300

    ansible-12

--- infra/packer/scripts/install_ruby.sh
+++ infra/packer/scripts/install_ruby.sh
@@ -2,6 +2,10 @@
 who
 sudo apt update
 sleep 30
+sudo apt install python-apt -y
+sleep 30
+sudo apt install ruby-bundler -y
+sleep 30
 sudo apt install gnupg2 -y
 sleep 30
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
